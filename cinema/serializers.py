e545e1c9127cac1be479b730cd8c5d4d0dc86fea
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-09-29 15:12:41 +0300

    change file serialize and view

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -9,10 +9,10 @@ class MovieSerializer(serializers.Serializer):
     description = serializers.CharField()
     duration = serializers.IntegerField()
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Movie:
         return Movie.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance, validated_data: dict) -> Movie:
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get(
             "description", instance.description
@@ -24,14 +24,15 @@ class MovieSerializer(serializers.Serializer):
 
 
 class CinemaHallSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=25)
     rows = serializers.IntegerField()
     seats_in_row = serializers.IntegerField()
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> CinemaHall:
         return CinemaHall.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: CinemaHall, validated_data: dict) -> CinemaHall:
         instance.name = validated_data.get("name", instance.name)
         instance.rows = validated_data.get("rows", instance.rows)
         instance.seats_in_row = validated_data.get(
@@ -44,12 +45,13 @@ class CinemaHallSerializer(serializers.Serializer):
 
 
 class GenreSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=25)
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Genre:
         return Genre.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: Genre, validated_data: dict) -> Genre:
         instance.name = validated_data.get("name", instance.name)
 
         instance.save()
@@ -58,13 +60,14 @@ class GenreSerializer(serializers.Serializer):
 
 
 class ActorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(max_length=63)
     last_name = serializers.CharField(max_length=63)
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Actor:
         return Actor.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: Actor, validated_data: dict) -> Actor:
         instance.first_name = validated_data.get(
             "first_name", instance.first_name
         )
--- cinema/views.py
+++ cinema/views.py
@@ -21,10 +21,9 @@ class GenreList(APIView):
 
     def post(self, request) -> Response:
         serializer = GenreSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class GenreDetail(APIView):
@@ -39,10 +38,9 @@ class GenreDetail(APIView):
     def put(self, request, pk) -> Response:
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, pk) -> Response:
         genre = self.get_object(pk)
@@ -52,10 +50,9 @@ class GenreDetail(APIView):
     def patch(self, request, pk) -> Response:
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ActorList(
